a21044e39b917b0544a746fdc3a7e00dfb5f0d13
Adrian-Revk <ra.dubouchet@gmail.com>
2017-03-24 21:39:02 -0400

    Dynamic DLL loading. File Write Time check for automatic reloading

--- radar.cpp
+++ radar.cpp
@@ -1,21 +1,64 @@
 #include "sun.h"
 #include <stdio.h>
 #include <windows.h>
+#include <string.h>
+
 
 struct game_code
 {
     HMODULE GameDLL;
+    FILETIME GameDLLLastWriteTime;
+    bool IsValid;
+
+    // DLL Dynamic Entry Points
     game_func *func1;
+
 };
 
-game_code LoadGameCode()
+// Note() : expect a MAX_PATH string as Path
+void GetExecutablePath(char *Path)
+{
+    HMODULE ExecHandle = GetModuleHandleW(NULL);
+    GetModuleFileNameA(ExecHandle, Path, MAX_PATH);
+
+    char *LastPos = strrchr(Path, '\\');
+    unsigned int NumChar = (unsigned int)(LastPos - Path) + 1;
+
+    // Null-terminate the string at that position before returning
+    Path[NumChar] = 0;
+}
+
+FILETIME FindLastWriteTime(char *Path)
+{
+    FILETIME LastWriteTime = {};
+
+    WIN32_FIND_DATA FindData;
+    HANDLE H = FindFirstFileA(Path, &FindData);
+    if(H != INVALID_HANDLE_VALUE)
+    {
+        LastWriteTime = FindData.ftLastWriteTime;
+        FindClose(H);
+    }
+
+    return LastWriteTime;
+}
+
+game_code LoadGameCode(char *DllSrcPath, char *DllDstPath)
 {
     game_code result = {};
 
-    result.GameDLL = LoadLibrary("sun.dll");
+    result.func1 = Func1Stub;
+
+    CopyFileA(DllSrcPath, DllDstPath, FALSE);
+
+    result.GameDLL = LoadLibraryA(DllDstPath);
+
     if(result.GameDLL)
     {
+        result.GameDLLLastWriteTime = FindLastWriteTime(DllSrcPath);
+
         result.func1 = (game_func*)GetProcAddress(result.GameDLL, "GameFunc");
+        result.IsValid = (result.func1 != NULL);
     }
 
     return result;
@@ -23,16 +66,58 @@ game_code LoadGameCode()
 
 void UnloadGameCode(game_code *code)
 {
-   FreeLibrary(code->GameDLL); 
+    if(code->GameDLL)
+    {
+        FreeLibrary(code->GameDLL); 
+    }
+
+    code->IsValid = false;
+    code->func1 = Func1Stub;
+}
+
+bool CheckNewDllVersion(game_code Game, char *DllPath)
+{
+    FILETIME WriteTime = FindLastWriteTime(DllPath);
+    if(CompareFileTime(&Game.GameDLLLastWriteTime, &WriteTime) != 0) {
+        Game.GameDLLLastWriteTime = WriteTime;
+        return true;
+    }
+
+    return false;
 }
 
 int main()
 {
-    game_code game = LoadGameCode();
-    int r = game.func1(12, 22);
-    printf("%d\n", r);
+    const char DllName[] = "sun.dll";
+    const char DllDynamicCopyName[] = "sun_temp.dll";
 
-    UnloadGameCode(&game);
+    char ExecFullPath[MAX_PATH];
+    char DllSrcPath[MAX_PATH];
+    char DllDstPath[MAX_PATH];
+
+    GetExecutablePath(ExecFullPath);
+    strcpy(DllSrcPath, ExecFullPath);
+    strcat(DllSrcPath, DllName);
+    strcpy(DllDstPath, ExecFullPath);
+    strcat(DllDstPath, DllDynamicCopyName);
+
+    game_code Game = LoadGameCode(DllSrcPath, DllDstPath);
+
+    while(true)
+    {
+        if(CheckNewDllVersion(Game, DllSrcPath))
+        {
+            UnloadGameCode(&Game);
+            game = LoadGameCode(DllSrcPath, DllDstPath);
+        }
+
+        int r = Game.func1(12, 22);
+        printf("%d\n", r);
+
+        Sleep(1000);
+    }
+
+    UnloadGameCode(&Game);
 
     return 0;
 }
